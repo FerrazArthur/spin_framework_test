4a40e49d4d0f08e94a2ead74c00d66896637a21e
Arthur Ferraz <arthursnc@gmail.com>
2023-09-26 15:44:42 -0300

    [feat] started spin server

---
+++ hello/hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("Content-Type: text/plain\n\n");
+    printf("Hello, World\n");
+    printf("This is a web server!\n");
+    printf("My name is Arthur\n");
+}
