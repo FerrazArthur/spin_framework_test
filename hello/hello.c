2901ca10b09337ae472c1acca3e80d91739c67c5
Arthur Ferraz <arthursnc@gmail.com>
2023-10-10 13:09:34 -0300

    [feat] Add package ci-cd

--- hello/hello.c
+++ hello/hello.c
@@ -5,5 +5,5 @@ int main() {
     printf("Content-Type: text/plain\n\n");
     printf("Hello, World\n");
     printf("This is a web server!\n");
-    printf("My name is Arthur\n");
+    printf("My name is arhtuhrr\n");
 }
